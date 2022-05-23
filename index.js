af76b66babae6407dc3251ac88aaee7f166acf96
Nayeem29 <nayeembracu29@gmail.com>
2022-05-24 05:12:09 +0600

    purchase post method created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const res = require('express/lib/response');
 app.use(express.json());
@@ -17,12 +17,35 @@ async function run() {
   await client.connect();
   try {
     console.log('database is connected');
+    const cameraCollection = await client.db('products').collection('camera');
+    const reviewCollection = await client.db('products').collection('review');
+    const purchaseCollection = await client.db('products').collection('purchase');
+
+    app.get('/camera', async (req, res) => {
+      const result = await cameraCollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/camera/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await cameraCollection.findOne(query);
+      res.send(result);
+    })
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+    app.post('/purchaseinfo', async (req, res) => {
+      const order = req.body;
+      const result = await purchaseCollection.insertOne(order);
+      res.send(result);
+    })
   } finally {
 
   }
 
 }
-run().catch(console.dir())
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('The server is running....');
